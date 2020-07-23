4d2d8a07f1668ff46fabb459474da3b3f9648e39
Sylvester <sbcardorelle@hotmail.co.uk>
2020-07-23 15:11:37 +0100

    Updates

---
+++ precache-manifest.5025e8d96e00ee8e88b71441bf768b90.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319711f01330d240145571b734de2f48",
+    "url": "/Cover/index.html"
+  },
+  {
+    "revision": "41cb20e5abe24115d306",
+    "url": "/Cover/static/css/main.ecec968c.chunk.css"
+  },
+  {
+    "revision": "a21ef6ef4a37f25f6a75",
+    "url": "/Cover/static/js/2.9279cd55.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Cover/static/js/2.9279cd55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41cb20e5abe24115d306",
+    "url": "/Cover/static/js/main.3086d57e.chunk.js"
+  },
+  {
+    "revision": "636c9e41c66089011e00",
+    "url": "/Cover/static/js/runtime-main.f988800a.js"
+  },
+  {
+    "revision": "af450edc0a94be7145300ce85ec26af1",
+    "url": "/Cover/static/media/Emphatic.af450edc.jpg"
+  },
+  {
+    "revision": "4e369292c60b97782db6447776ca2bf1",
+    "url": "/Cover/static/media/cover.4e369292.png"
+  },
+  {
+    "revision": "bc3c5f341c09ef9b70151b0a6d2ae825",
+    "url": "/Cover/static/media/inventive.bc3c5f34.jpg"
+  },
+  {
+    "revision": "0ba71c6ce121530eab0aacea3ca8a76b",
+    "url": "/Cover/static/media/passion.0ba71c6c.jpg"
+  }
+]);
